213b4f7f5cbef31e657bdbf89f23316fb3957eca
ganafaye <ganafaye88@gmail.com>
2025-06-02 15:46:15 +0000

    version: 1

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  @vite('resources/css/app.css')
+</head>
+<body>
+  <div class="container">
+    <h1 class="text-primary text-3xl font-bold underline">
+      Hello, Laravel avec Bootstrap et Tailwind !
+    </h1>
+  </div>
+  @vite('resources/js/app.js')
+</body>
+</html>
